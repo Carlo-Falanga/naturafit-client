4f271ea4f3d84d71a1c1074f8c5733ad7b8dc0e0
Carlo-Falanga <carlofalanga7@gmail.com>
2026-07-16 13:03:24 +0200

    Wrap App component with BrowserRouter for routing support

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import * as bootstrap from "bootstrap";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
   </StrictMode>,
 );
